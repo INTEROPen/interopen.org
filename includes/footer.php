41ab1de4e27478255a76ce790a2ef63701843506
jonnyry <jonny@Jonnys-MacBook-Pro.local>
2016-06-29 17:19:19 +0100

    Add GitHub button link to footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,16 @@
       <hr/>
 
       <footer>
-        <p>&copy; 2016 INTEROPen</p>
+        <span>&copy; 2016 INTEROPen</span>
+        
+        <span style="float: right;">
+            <span class="github-btn" id="github-btn">
+                <a class="gh-btn" id="gh-btn" href="https://github.com/INTEROPen/" target="_blank" aria-label="">
+                    <span class="gh-ico" aria-hidden="true"></span>
+                    <span class="gh-text" id="gh-text">GitHub</span>
+                </a>
+            </span>
+        </span>
       </footer>
       
     </div>

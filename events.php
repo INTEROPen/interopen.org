e37ab8b5605ebc9a7f6f2e21cfce0b0eaa65a05b
jonnyry <jonny@Jonnys-MacBook-Pro.local>
2016-07-06 19:13:14 +0100

    Update Interopen4 event documents and rename files for consistency

--- events.php
+++ events.php
@@ -14,11 +14,11 @@
         <div class="event-location"><img src="images/world.png" alt="location"/><a href="https://www.google.co.uk/maps/place/TPP/@53.8434217,-1.6286051,15z/data=!4m5!3m4!1s0x0:0xe9ab6533c44a1e6c!8m2!3d53.8434217!4d-1.6286051">TPP, Leeds</a></div>
         <p><i>Registration for this event is now closed.</i></p>
         
-        <div class="event-document"><p><a href="content/Interopen4%20-%20Draft%20Agenda.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Draft agenda</a></p></div>
+        <div class="event-document"><p><a href="content/IO4%20-%20Draft%20Agenda.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Draft agenda</a></p></div>
         
         <p>Item four on the agenda is to consider a patient story to underpin the cross care setting interactions. The patient story is contained within the two documents below and will be floated for consideration at INTEROPen 4:</p>
         
-        <div class="event-document"><a href="content/Michael's%20Story%20-%20Introduction.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Cross care domain use case - Michael's story - introduction</a></div>
+        <div class="event-document"><a href="content/IO4%20-%20Michael's%20Story%20-%20Introduction.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Cross care domain use case - Michael's story - introduction</a></div>
         <!--<div class="event-document"><a href="content/Michael's%20Story%20-%20Complex%20Care%20(Interop).pdf"><img src="images/page_pdf.png" alt="PDF document"/>Cross care domain use case - Michael's story - story boards</a></div>-->
       </td>
     </tr>
@@ -47,7 +47,7 @@
           <p>Follow on meeting of the group now consisting of Orion, IMS Maxims, Endeavour, EMIS, Cerner, Intersystems, Black Pear, HSCIC, NHS England, TPP</p>
           <p><img src="images/time.png" alt="time"/>10:00 - 14:00</p>
           <div class="event-location"><a href="https://goo.gl/maps/8hXtfnCxHS22"><img src="images/world.png" alt="location"/>Orion Health, London</a></div>
-          <p><a href="content/C4HI-SG-%20Minutes%2015-4-16-%20final.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Minutes of Interop supplier subgroup meeting</a></p>
+          <p><a href="content/IO2%20-%20C4HI-SG-%20Minutes%2015-4-16-%20final.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Minutes of Interop supplier subgroup meeting</a></p>
         </td>
       </tr>
       <tr>
@@ -56,7 +56,7 @@
           <p>Inaugural meeting with early members, Orion, IMS Maxims, Endeavour, EMIS, Cerner, Intersystems, Black Pear HSCIC, NHS England</p>
           <p><img src="images/time.png" alt="time"/>9:00 - 13:00</p>
           <div class="event-location"><a href="https://goo.gl/maps/eK1EaFsZaGs"><img src="images/world.png" alt="location"/>Tech UK, London</a></div>
-          <p><a href="content/Outcomes%20from%20meeting%20of%2018th%20March%202016.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Outcomes from Supplier Group meeting document</a></p>
+          <p><a href="content/IO1%20-%20Outcomes%20from%20meeting%20of%2018th%20March%202016.pdf"><img src="images/page_pdf.png" alt="PDF document"/>Outcomes from Supplier Group meeting document</a></p>
         </td>
       </tr>
     </tbody>

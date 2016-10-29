96b3655e81f192791cd732c3736e9575a9538192
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2016-10-29 11:04:42 +0100

    Add latest version of CareConnect profiles

--- resource-profiles.php
+++ resource-profiles.php
@@ -19,7 +19,7 @@
     <h3>Member proposals</h3>
     
     <ol>
-      <li><a href="candidate-profiles/convergence">The Convergence Group - Proposals for AllergyIntolerance, Condition, MedicationOrder, MedicationStatement, Patient, Observation</a></li>
+      <li><a href="candidate-profiles/convergence/">The CareConnect Group - Proposals for AllergyIntolerance, Condition, MedicationOrder, MedicationStatement, Patient, Observation</a></li>
     </ol>
     
     <h3>Other resouces</h3>

f6ab3b571da40e782aa4b661f480a6c897eb8c35
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2016-11-14 14:02:19 +0000

    Add InteropSummit event

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
             "events.php" => array("NAME" => "Meetings &amp; events", "LINK" => "events"),
             "news.php" => array("NAME" => "News &amp; media", "LINK" => "news"),
             "discuss.php" => array("NAME" => "Discuss", "LINK" => "discuss"),
-            "interopsummit.php" => array("NAME" => "<b><span style='color: #1b5a7d'>@INTEROP</span><span style='color: #F16D3B'>SUMMIT</span></b>", "LINK" => "interop-summit"));
+            "interop-summit.php" => array("NAME" => "<b><span style='color: #1b5a7d'>@INTEROP</span><span style='color: #F16D3B'>SUMMIT</span></b>", "LINK" => "interop-summit"));
 
   function writeNavbarItem($page_filename, $page_name, $page_link, $current_filename)
   {

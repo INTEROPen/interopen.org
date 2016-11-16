a6485d54d54342c5925288eba17e6edd1d5fca99
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2016-11-16 09:48:14 +0000

    Update resource profiles page and care connect profiles view

--- resource-profiles.php
+++ resource-profiles.php
@@ -6,21 +6,21 @@
 
     <h3>Overview</h3>
 
-    <p>This section references profile collections that have been submitted by INTEROPen members or member groups for consideration and the overall submission for further validation by other stakeholders.</p> 
+    <p>This section references profile collections that have been submitted by INTEROPen members or member groups for consideration and further validation by other stakeholders.</p> 
     <p>Any group member or group of members can submit members proposals subject to the following:</p>
 
     <ol>
       <li>They should be created using the Forge tool.</li>
-      <li>The must first be submitted via a pull request to the <a href="https://github.com/HL7-UK/CareConnect-profiles">HL7 CareConnect repository</a> to ensure visibility and access to the source.</li>
-      <li>A project on Simplifier should be created in order to be able to view the profiles, together with any documentation providing rationale for changes to the current version.</li>
+      <li>They must first be submitted via a pull request to the <a href="https://github.com/HL7-UK/CareConnect-profiles">HL7 CareConnect repository</a> to ensure visibility and access to the source.</li>
+      <li>A suitable view of the FHIR profile (e.g. Simplifier viewer, or via clinFHIR) should be created in order to be able to view the profiles, together with any documentation providing rationale for changes to the current version.</li>
       <li>A submission to this web site should include a summary of the project content and the organisations responsible for editing the proposal.</li>
     </ol>
     
     <h3>Member proposals</h3>
     
     <ol>
-      <li><a href="candidate-profiles/convergence/"><span style="font-size: 120%">The CareConnect Group</span></a>
-        <div style="padding-top: 10px;">Available for preview (<a href="candidate-profiles/convergence/">HERE</a>):</div>
+      <li><a href="candidate-profiles/care-connect/"><span style="font-size: 120%">The CareConnect Group</span></a>
+        <div style="padding-top: 10px;">Available for preview (<a href="candidate-profiles/care-connect/">HERE</a>):</div>
         <ul class="candidate-profile-name-list">
           <li>AllergyIntolerance</li>
           <li>Condition</li>

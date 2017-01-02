6a50c741dd4f8793232d4d75b91387dbdf841d8f
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-01-02 16:02:19 +0000

    Update interop summit newsflash to v14

--- interop-summit.php
+++ interop-summit.php
@@ -2,7 +2,7 @@
 
   <div style="text-align: center; width: 100%; padding: 20px;">
     <p style="font-size: 16px;"><a href="http://bit.ly/interop2017">Register your interest</a> &nbsp;| &nbsp;<a href="https://twitter.com/InteropSummit">@InteropSummit on Twitter</a></p>
-    <p><a href="http://bit.ly/interop2017"><img style="width: 800px;" src="images/Interop Summit newsflash v13-1.png"/></a></p>
+    <p><a href="http://bit.ly/interop2017"><img style="width: 800px;" src="images/Interop%20Summit%20newsflash%20v14.png"/></a></p>
   </div>
 
 <?php include('includes/footer.php') ?>

258a6021d081cd4ace238ee21ef5d88182b4c687
Matt Stibbs <git@stibbsy.co.uk>
2017-07-13 22:55:57 +0100

    Update members list

--- about.php
+++ about.php
@@ -27,15 +27,19 @@
       <div class="panel-heading">Our members</div>
       <div class="panel-body">
         <ul class="list-unstyled">
+          <li>AccuRx</li>
           <li>Active medicine</li>
-          <li>Advanced Health and Care</li>
+          <li>Advanced Health &amp; Care</li>
           <li>A|D Vault, Inc.</li>
           <li>Allocate Software</li>
           <li>Allscripts</li>
           <li>Answer Digital</li>
+          <li>Application Insight</li>
           <li>Atos</li>
+          <li>BCS (British Computer Society)</li>
+          <li>Birmingham &amp; Solihull LDR</li>
           <li>Birmingham &amp; Solihull Mental Health NHS FT</li>
-          <li>Blackpear</li>
+          <li>Black Pear</li>
           <li>Boots</li>
           <li>BT</li>
           <li>Cambio+ Healthcare Systems</li>
@@ -44,12 +48,18 @@
           <li>CGI Group</li>
           <li>CharityLog</li>
           <li>CheckWare</li>
+          <li>Clinisys</li>
           <li>Clinical Information</li>
           <li>CommonTime</li>
+          <li>Connecting Care Partnership</li>
+          <li>CSC</li>
           <li>Cyber Media Solutions</li>
           <li>Deepmind Health</li>
           <li>Deontics</li>
+          <li>Digital Health CCIO Network</li>
+          <li>Digital Health CIO Network</li>
           <li>Docman</li>
+          <li>Dorset Health &amp; Care Community (Dorset CCG)</li>
           <li>DXC Technology</li>
           <li>Egress</li>
           <li>Elsevier</li>
@@ -58,12 +68,16 @@
           <li>Endeavour Health</li>
           <li>Enovacom</li>
           <li>Epic</li>
+          <li>Epro</li>
           <li>everyLIFE Technologies</li>
           <li>Experis</li>
           <li>Forcare</li>
           <li>FreshEHR</li>
           <li>GPC Group</li>
           <li>Graphnet Health</li>
+          <li>Greater Manchester Health &amp;Social Care Partnership CDO</li>
+          <li>Great North Care Record</li>
+          <li>HCI Group (Tech Mahindra Ltd)</li>
           <li>Health Fabric</li>
           <li>Health Intelligence</li>
           <li>Health Linked</li>
@@ -71,10 +85,12 @@
           <li>HealthForge</li>
           <li>Healthlinked</li>
           <li>Healthy London Partnership</li>
+          <li>HL7 UK</li>
           <li>Huawei Technologies</li>
           <li>IBM</li>
-          <li>IHE</li>
+          <li>IHE UK</li>
           <li>IMD-SOFT</li>
+          <li>Imperial College Healthcare NHS Trust</li>
           <li>IMS Maxims</li>
           <li>INPS</li>
           <li>Intelesant</li>
@@ -83,23 +99,38 @@
           <li>Kainos Evolve</li>
           <li>Mayden</li>
           <li>Medelinked</li>
+          <li>Medical Connections</li>
           <li>Medxnote</li>
           <li>Microsoft Health &amp; Life Sciences</li>
-          <li>NHS DIGITAL</li>
-          <li>NHS ENGLAND</li>
+          <li>MphRx</li>
+          <li>NHS Digital</li>
+          <li>NHS England</li>
+          <li>NHS Gloucestershire CCG</li>
+          <li>NHS Hack Day</li>
+          <li>NHS South, Central, and West CSU</li>
+          <li>NHS Wales Informatics Service</li>
+          <li>NoemaLife UK</li>
+          <li>Northern Ireland CCIO Network</li>
+          <li>North Yorkshire County Council (Adult Social Care Services)</li>
+          <li>Nottinghamshire Health Informatics Service (NHIS)</li>
           <li>Northern Ireland CCIO network</li>
           <li>Nth Dimension</li>
           <li>OLM systems</li>
           <li>Open Mapping Software</li>
+          <li>OpenEHR</li>
           <li>OpenHealthHub CIC</li>
           <li>Optum International</li>
+          <li>Oracle</li>
           <li>Orion Health</li>
+          <li>OTTech Healthcare Consultancy</li>
           <li>Patients Know Best</li>
           <li>Pinnacle Health Partnership</li>
+          <li>PRSB (Professional Records Standards Body)</li>
           <li>Prederi</li>
           <li>psHEALTH</li>
           <li>Pumpco</li>
           <li>Quicksilva Systems Integration Solutions</li>
+          <li>Ramsay Systems</li>
           <li>Red Embedded Systems</li>
           <li>Researcher</li>
           <li>Restart Consulting</li>
@@ -107,19 +138,27 @@
           <li>Salford Royal NHS FT</li>
           <li>Servelec</li>
           <li>SiteKit</li>
+          <li>SOCITM</li>
           <li>Solutions4Health</li>
+          <li>Somerset Integrated Digital Electronic Record</li>
           <li>Specialist Computer Centres</li>
           <li>Stalis</li>
           <li>Sunquest</li>
           <li>SystemC</li>
           <li>Targett Business Technology</li>
+          <li>techUK</li>
           <li>Tiani Spirit</li>
+          <li>Tower Hamlet/Newham/City Hackney/Waltham Forest CCG</li>
           <li>TotalMobile</li>
           <li>TPP</li>
+          <li>Trisoft</li>
           <li>University College London Hospitals NHS FT</li>
           <li>University Southampton Hospitals NHS FT</li>
           <li>Virgin Media Business</li>
+          <li>Vision Health</li>
+          <li>Wolters Kluwer Health "HealthLanguage"</li>
           <li>Wiggly-Amps</li>
+          <li>X-Lab</li>
           <li>XML Solutions</li>
           <li>York Teaching Hospitals NHS FT</li>
         </ul>

019e828b2d87edaac5b6d1fb91404caae31a61eb
Tohmua <oliver.musgrove-wethey@mayden.co.uk>
2017-08-23 19:15:57 +0100

    Initial Wordpress Install

--- README.md
+++ README.md
@@ -7,18 +7,20 @@
 1. Get a clean and up-to-date copy of the website from the GitHub repository:
 
     https://github.com/INTEROPen/interopen.org.git
-    
-2. Make changes to the site on your local machine
 
-3. Commit and push them back to GitHub (on the master branch)
+2. Copy the `wp-config-sample.php` to `wp-config.php` and update with local settings.
 
-4. If you have permission, to deploy to the live site:
-    
-    * Connect to the live web server via FTP 
+3. Make changes to the site on your local machine
+
+4. Commit and push them back to GitHub (on the master branch)
+
+5. If you have permission, to deploy to the live site:
+
+    * Connect to the live web server via FTP
     * Copy your local copy of the files to the `/site/wwwroot` directory
-    
+
 
 ### Rewrite Rules
 If you want to add a rewrite rule for the web server, you need to add it to both the .htaccess file, and the web.config file.
 
-Azure Hosting will use the web.config file, but Apache-based servers will use .htaccess. 
+Azure Hosting will use the web.config file, but Apache-based servers will use .htaccess.
--- index.php
+++ index.php
@@ -1,85 +1,17 @@
-<?php include('includes/header.php') ?>
-      
-    <div class="jumbotron">
-      <div class="container">
-        
-        <p>Leading organisations and individuals have come together to create INTEROPen, an action group to accelerate the development of open standards for interoperability in the health and social care sector.</p>
-        <p><span><a class="btn btn-primary btn-lg" href="about" style="width:13em" role="button">Learn more &raquo;</a><span>&nbsp;</span><a class="btn btn-primary btn-lg" href="content/MembershipTnCINTEROPenWebsite.pdf" style="width:1sem" role="button">Membership Terms &raquo;</a></span></p>
-      </div>
-    </div>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-    <div class="row">
-      <div class="container">
-      <div class="col-md-4 index-col">
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-        <h2 class="index-col-heading">Upcoming events</h2>          
-
-         <!-- <div class="event">
-          <span class="event-date">2 &amp; 3 Mar</span>
-          <span class="event-text"><a href="events#interopsummit">Interop Summit 2017 </a><a href=https://t.co/omn0nlEn45>BOOK HERE</a> </span>
-        </div>
-          
-        <div class="event">
-          <span class="event-date">20 Jan</span>
-          <span class="event-text"><a href="events#catonfhir">CATonFHIR hands-on event</a></span>
-        </div>-->
-        
-        <p><a class="btn btn-default" href="events" role="button">View full events list &raquo;</a></p>
-
-        <div class="index-interopen4-box">
-          <div class="index-interopen4-box-image img-rounded">
-            <table>
-              <tr>
-                <td>
-                  <p>INTEROPen 4 was held on the 5 July</p>
-                </td>
-              </tr>
-              <tr>
-                <td class="index-interopen4-box-bottom">
-                  <p><a href="events#interopen4">View slides from the event</a></p>
-                </td>
-              </tr>
-            </table>
-            
-          </div>
-        </div>
-
-      </div>
-
-      <div class="col-md-4 index-col">
-
-        <h2 class="index-col-heading">News</h2>
-        <p class="index-news-item"><span class="news-date">28 Apr</span><a href="news#28apr">INTEROPen Governance proposals from interoperability networks</a></p>
-        <p class="index-news-item"><span class="news-date">14 Mar</span><a href="news#14mar">Request for feedback on the INTEROpen Governance structure</a></p>
-        <p class="index-news-item"><span class="news-date">27 Feb</span><a href="news#27feb">INTEROPen request a HL7UK ballot on the draft INTEROPen CareConnect profiles(extensions and valuesets)</a></p>
-        <p class="index-news-item"><span class="news-date">12 Jan</span><a href=https://docs.google.com/document/d/1-ZaZw5Y69cNigQ4iPb2pr6Qz4Ksp6Tf-anaCEfNaLsk/edit>Interop 2017 Summit agenda</a> </p>
-        <p class="index-news-item"><span class="news-date">17 Jan</span><a href="news#17jan">Code4Health Interoperability Community Board Response to Emerging Target Architecture paper by Will Smart, NHS England CIO</a></p>
-        <p class="index-news-item"><span class="news-date">16 Jan</span><a href="news#16jan">FHIR Briefing Paper - FHIR Technical Tools to Code4Health Interop Community Board</a></p>        
-
-        <p><a class="btn btn-default" href="news" role="button">View all news &raquo;</a></p>
-        
-        <h2 class="index-col-heading">In the media</h2>
-
-        <p class="index-news-item"><span class="news-date">05 Jan</span><a href="news#05jan">2017 emerges as pivotal year for FHIR interoperability standard</a></p>        
-        <p class="index-news-item"><span class="news-date">3 Nov</span><a href="news#3nov">Aide Memoire: all mod cons</a></p>    
-        <p class="index-news-item"><span class="news-date">6 Sep</span><a href="news#6sep">Calling the FHIR brigade</a></p>
-        <p class="index-news-item"><span class="news-date">17 Aug</span><a href="news#17aug">Aide Memoire: how long?</a></p>
-        <p class="index-news-item"><span class="news-date">15 Aug</span><a href="news#15aug">Fantastic digital developments as we aim to go paperless</a></p>
-        <p class="index-news-item"><span class="news-date">23 Jun</span><a href="news#23jun">INTEROPen accelerates development of NHS interoperability standards</a></p>
-
-        <p><a class="btn btn-default" href="news#media" role="button">View all in the media &raquo;</a></p>
-
-     </div>
-
-      <div class="col-md-4 index-col">
-        <div class="twitter-box">
-          <h2 class="index-col-heading">Tweets</h2>
-          <a class="twitter-timeline" data-width="300" data-height="400" href="https://twitter.com/INTEROPenAPI" data-chrome="noheader nofooter"></a> 
-        </div>
-      </div>
-    </div>
-    </div>
-
-    <script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>
-
-<?php include('includes/footer.php') ?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
